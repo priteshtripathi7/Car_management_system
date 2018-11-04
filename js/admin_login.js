9c123d2d8b044d803abe8aef11f2980efc6b172a
subhamagrawal7 <subhamagrawal7@gmail.com>
2018-11-04 23:16:21 +0530

    Updated Tables and login pages
    1. Added Cars to admin table
    2. Created user cars table
    3. Created Admin Login page
    4. Created User login page and added link to create account section

--- js/admin_login.js
+++ js/admin_login.js
@@ -1,6 +1,7 @@
 function validate(){
-    if(   document.getElementById("username").value === "admin@xyz.com"
-       && document.getElementById("password").value == "admin" )
+    if((   document.getElementById("username").value === "user@yahoo.com"
+       && document.getElementById("password").value == "user1" ) || (document.getElementById("username").value === "user2@gmail.com"
+       && document.getElementById("password").value == "user2" ))
     {
         alert( "VALIDATION SUCCESS!!!!");
     }
