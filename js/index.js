22a4bc38d8d6a0393cf4d33ae71aa896a60f85ac
Pritesh Kumar Tripathi <pritesh.kt@gmail.com>
2018-11-05 22:15:26 +0530

    header completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-function dispalyNavmenu(){
-	let btn = document.getElementById('hide');
-	btn.classList.add("hidden-grid");
-	btn.classList.remove("hidden-none");
+	function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
