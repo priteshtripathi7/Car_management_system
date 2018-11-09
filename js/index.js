e33d1d6a593c4afd5c3517a19e85ae627aa28f39
Pritesh Kumar Tripathi <pritesh.kt@gmail.com>
2018-11-09 15:20:58 +0530

    header added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-	function classToggle() {
-  const navs = document.querySelectorAll('.Navbar__Items')
-  
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items');
+  const back = document.querySelector('.Navbar');
   navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
 document.querySelector('.Navbar__Link-toggle')
