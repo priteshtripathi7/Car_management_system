75ce49bace1ae24f8cb4e274e72baa96d88b8352
subhamagrawal7 <subhamagrawal7@gmail.com>
2018-11-15 13:29:20 +0530

    Made booking pages
    Left to add header and footer in book.html
    
    Made changes to admin login and user login headers so that animation shows up

---
+++ book.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd<10){
+    dd='0'+dd;
+} 
+if(mm<10){
+    mm='0'+mm;
+} 
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("somedate").setAttribute("min", today);
